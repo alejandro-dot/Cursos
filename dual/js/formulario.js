e3c4d0f0bff28692ca7cec15d414decc459fee42
alejandro-dot <palizadaalex@gmail.com>
2020-09-29 17:32:30 -0500

    si

--- dual/js/formulario.js
+++ dual/js/formulario.js
@@ -1,1 +1,14 @@
-alert('hola');
+//alert('oda');
+var formulario= document.getElementById('formulario');
+var nombre = formulario.nombre;
+ var sexo = formulario.sexo;
+ var terminos = formulario.terminos;
+
+function validar(e){
+  if(nombre.value>18){
+    alert('mucho texto');
+    e.preventDefault;
+
+  }
+}
+formulario.addEventListener('submit', validar);
