996636ccc8e62ef25c5574adc620f430ae6405f2
alejandro-dot <palizadaalex@gmail.com>
2020-09-30 10:57:14 -0500

    se hizo el primer formulario sencillo

--- dual/js/formulario.js
+++ dual/js/formulario.js
@@ -1,14 +1,2 @@
 //alert('oda');
-var formulario= document.getElementById('formulario');
-var nombre = formulario.nombre;
- var sexo = formulario.sexo;
- var terminos = formulario.terminos;
-
-function validar(e){
-  if(nombre.value>18){
-    alert('mucho texto');
-    e.preventDefault;
-
-  }
-}
-formulario.addEventListener('submit', validar);
+//var formulario= document.getElementsByName('formulario');
